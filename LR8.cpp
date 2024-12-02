1cfc5235b5d45aa87d3e45f3e47e8d7544687fe4
Baranov Sergiy <onlykomzypc@gmail.com>
2024-12-02 14:19:48 +0200

    add new file

---
+++ LR8.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+using namespace std;
+int main()
+{
+    float x, Z, dob;
+    cout << "Enter x: "; cin >> x;
+    dob = 1;
+    for (int n = 1; n <= 5; n++) {
+        dob *= log(n * x);
+    }
+    Z = tan(x) + (dob);
+    cout << " Z = " << Z << endl;
+    system("pause");
+    return 0;
+}
